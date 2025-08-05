d07e3efbc0de7c9851738c27f81814a7840e5b4e
Grace Amondi <miswa.grace@gmail.com>
2025-08-05 08:50:57 +0300

    delete old files

--- backup.sh
+++ backup.sh
@@ -26,6 +26,6 @@ rclone $RCLONE_FLAGS copy "${ARCHIVE_FILE}" "${REMOTE}" \
 
 # Delete old files from remote (>3 days old)
 echo "Deleting old files from remote ..."
-rclone $RCLONE_FLAGS delete --min-age 2d "${REMOTE}" --drive-use-trash=false
+rclone $RCLONE_FLAGS delete --min-age 2d  --drive-use-trash=false "${REMOTE}"
 
 echo "Sync complete."
